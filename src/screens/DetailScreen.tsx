dc124c04cb17a4567de232bc6e9ca83faa8fc38a
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-13 14:02:14 -0600

    Feat. Show image and titles

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,18 +1,53 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { View, Text } from 'react-native';
-// import { Movie } from '../interfaces/movieInterface';
+import { ScrollView, View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { RootStackParams } from '../navigation/StackNavigator';
+
+const screenHeight = Dimensions.get('screen').height;
 
 interface DetailsScreenProps extends StackScreenProps<RootStackParams, 'DetailScreen'> { }
 
 export const DetailScreen = ({ route }: DetailsScreenProps) => {
 
     const movie = route.params;
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
     return (
-        <View>
-            <Text>DetailScreen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={{ uri }}
+                    style={styles.posterImage}
+                />
+            </View>
+            <View style={styles.marginContainer}>
+                <Text style={styles.subtitle}>{movie.original_title}</Text>
+                <Text style={styles.title}>{movie.title}</Text>
+            </View>
+        </ScrollView>
+
     );
 };
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        width: '100%',
+        height: screenHeight * 0.7,
+    },
+    posterImage: {
+        flex: 1,
+    },
+    marginContainer: {
+        marginHorizontal: 20,
+        marginTop: 20,
+    },
+    subtitle: {
+        fontSize: 18,
+        color: '#212121',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+});
