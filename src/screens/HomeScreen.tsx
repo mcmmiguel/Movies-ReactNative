fb6884590a8d5e361b5edb0febfa39d535a3af8c
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-12 19:38:24 -0600

    Feat. Implement Carousel

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useMovies } from '../hooks';
 import { globalStyles } from '../../styles';
 import { MoviePoster } from '../components';
+import Carousel from 'react-native-snap-carousel';
+
+const { width: windowWidth } = Dimensions.get('window');
 
 export const HomeScreen = () => {
 
@@ -20,9 +23,20 @@ export const HomeScreen = () => {
 
     return (
         <View style={{ marginTop: top + 20 }}>
-            <MoviePoster
-                movie={nowPlayingMovies[0]}
-            />
+            <View style={styles.carouselContainer}>
+                <Carousel
+                    data={nowPlayingMovies}
+                    renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                    sliderWidth={windowWidth}
+                    itemWidth={300}
+                />
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    carouselContainer: {
+        height: 440,
+    },
+});
